e5330c65bcc7c01866b15d7ea2315c2a024c81be
Obi-sloak <odoemeneobinna@gmail.com>
2024-03-22 05:49:17 -0700

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,60 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+
 """
+Connects to MySQL, prints the State object with the specified
+name from the specified database.
+
+Returns:
+    None. Prints the id of the State object with the specified
+    name to the console, or "Not found" if not found.
+"""
+
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def print_state_by_name(username, password, database, state_name):
+    """
+    Connects to MySQL, prints the State object with the specified
+    name from the specified database.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Name of the MySQL database containing
+        the State objects.
+        state_name (str): Name of the state to search for.
+
+    Returns:
+        None. Prints the id of the State object with the specified
+        name to the console, or "Not found" if not found.
+    """
+
+    engine = create_engine(
+        f'mysql://{username}:{password}@localhost:3306/{database}')
+
+    Base.metadata.bind = engine
+
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
 
 
 if __name__ == "__main__":
-     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                             .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-     Base.metadata.create_all(engine)
-     Session = sessionmaker(bind=engine)
-     session = Session()
-     instance = session.query(State).filter(State.name == (sys.argv[4],))
-     try:
-         print(instance[0].id)
-     except IndexError:
-         print("Not found")
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    print_state_by_name(username, password, database, state_name)
