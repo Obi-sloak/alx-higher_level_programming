94d2cc5a33b05c58f0806438a3ccdd458ed1f412
Obi-sloak <odoemeneobinna@gmail.com>
2024-03-22 07:25:10 -0700

    updated

--- 0x0F-python-object_relational_mapping/model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/model_state_fetch_first.py
@@ -1,20 +1,58 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
+
 """
+Connects to MySQL, prints the first State object
+from the specified database.
+
+Returns:
+    None. Prints the first State object to the
+    console.
+"""
+
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def print_first_state(username, password, database):
+    """
+    Connects to MySQL, prints the first State object
+    from the specified database.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Name of the MySQL database
+        containing the State objects.
+
+    Returns:
+        None. Prints the first State object to the
+        console.
+    """
+
+    engine = create_engine(
+        f'mysql://{username}:{password}@localhost:3306/{database}')
+
+    Base.metadata.bind = engine
+
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+
+    session.close()
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    instance = session.query(State).first()
-    if instance is None:
-        print("Nothing")
-    else:
-        print(instance.id, instance.name, sep=": ")
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    print_first_state(username, password, database)
